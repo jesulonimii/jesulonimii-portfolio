a6aeeb66d18d7b5e8bda763e743332948dba5759
Jesulonimii <jesulonimii.will@gmail.com>
2023-09-06 06:53:09 +0100

    Added AI Chatbot

--- app/providers.tsx
+++ app/providers.tsx
@@ -7,6 +7,7 @@ import { IconlyProvider } from "react-iconly";
 import { blobityConfig, COLORS } from "@utils"
 import useBlobity from "blobity/lib/react/useBlobity";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Script from "next/script"
 
 const queryClient = new QueryClient();
 
@@ -38,6 +39,15 @@ const Providers = ({ children }) => {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<DarkTheme.Provider value={{ darkMode, setDarkMode }}>
+
+				<Script
+					src="https://app.aichatbot.so/scripts/aichatbotso-widget.js"
+					type="text/javascript"
+					data-name="aichatbotso"
+					data-address="https://app.aichatbot.so"
+					data-id="6892cecd-091a-41fc-9352-3694fff797df" >
+				</Script>
+
 				{/*@ts-ignore*/}
 				<IconlyProvider
 					set="light"
